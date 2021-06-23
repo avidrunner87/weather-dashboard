32e3d0db496c31a1b599aeace7f2f183cd2b7150
avidrunner87 <36962520+avidrunner87@users.noreply.github.com>
2021-06-23 09:45:28 -0500

    Enabled historical searches as well as new searches to show up in the results

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -26,7 +26,7 @@ function renderMainContent() {
     // Build the Search column
     let $searchColumnDiv = $("<div>");
     $searchColumnDiv.attr("id", "search");
-    $searchColumnDiv.addClass("col-sm-3 col-md-3 col-lg-3");
+    $searchColumnDiv.addClass("col-md-3");
     
     // Build Search column title
     let $searchTitleH2 = $("<h2>");
@@ -44,17 +44,22 @@ function renderMainContent() {
     $searchInput.attr("placeholder", "Houston, TX");
     $searchInput.addClass("textarea form-control");
 
+    let $searchInputId = $("<input>");
+    $searchInputId.attr("id", "searchInputId");
+    $searchInputId.attr("type", "hidden");
+
     let $searchInputLAT = $("<input>");
     $searchInputLAT.attr("id", "searchInputLAT");
     $searchInputLAT.attr("type", "hidden");
 
-    let $searchInputLONG = $("<input>");
-    $searchInputLONG.attr("id", "searchInputLNG");
-    $searchInputLONG.attr("type", "hidden");
+    let $searchInputLNG = $("<input>");
+    $searchInputLNG.attr("id", "searchInputLNG");
+    $searchInputLNG.attr("type", "hidden");
 
     $searchInputDiv.append($searchInput);
+    $searchInputDiv.append($searchInputId);  
     $searchInputDiv.append($searchInputLAT);
-    $searchInputDiv.append($searchInputLONG);
+    $searchInputDiv.append($searchInputLNG);
     $searchColumnDiv.append($searchInputDiv);
 
     // Build Search button for text box
@@ -84,13 +89,18 @@ function renderMainContent() {
     // Append Search to main content
     $("main").append($searchColumnDiv);
 
+    // Build the Results column
+    let $resultsColumnDiv = $("<div>");
+    $resultsColumnDiv.attr("id", "results");
+    $resultsColumnDiv.addClass("col-md-9");
+
+    // Append the Results to the main content
+    $("main").append($resultsColumnDiv);
+
     // Render Search history
     renderSearchHistory();
 
     searchInput = document.getElementById("searchInput");
-
-    // Build the Results column
-
 }
 
 // Create the Search history
@@ -104,16 +114,17 @@ function renderSearchHistory() {
 
     // TODO: Fix the sort on the searchItems array
     searchItems.sort(function(a, b) {
-        return a.searchCity - b.searchCity
+        return a.cityName - b.cityName
     });
 
     for (let i = 0; i < searchItems.length; i++) {
         let $searchHistoryButton = $("<button>");
         $searchHistoryButton.attr("id", `searchHistory_item${i}`);
+        $searchHistoryButton.attr("cityId", searchItems[i].cityId);
         $searchHistoryButton.addClass("button searchHistory");
     
         let $searchButtonSpan = $("<span>");
-        $searchButtonSpan.text(searchItems[i].searchCity);
+        $searchButtonSpan.text(searchItems[i].cityName);
 
         $searchHistoryButton.append($searchButtonSpan);
 
@@ -127,6 +138,24 @@ function searchInputAutoComplete() {
     autocomplete = new google.maps.places.Autocomplete(searchInput, {
         types: ['geocode']
     });
+}
+
+// Render the Results column
+function renderResults(cityId) {
+    let searchItems = JSON.parse(localStorage.getItem("weatherDashboard_searchItems"));
+
+    for (let i = 0; i < searchItems.length; i++) {
+        if (searchItems[i].cityId === cityId) {
+            // Build Result column title
+            $("#results").empty();
+            let $resultsTitleH2 = $("<h2>");
+            $resultsTitleH2.addClass("display-6");
+            $resultsTitleH2.text(searchItems[i].cityName);
+
+            // Append the results to the Results column
+            $("#results").append($resultsTitleH2);
+        }
+    }
 }
 
 // Calls init to retrieve data and render it to the page on load
@@ -143,44 +172,58 @@ document.querySelector("#search").addEventListener("click", function(event) {
             searchItems = [];
         }
 
+        let cityId = $(`#${id}`).attr("cityId");
+
         // Set the new entry for the array
-        let searchCity = document.getElementById("searchInput").value.trim();
-        let cityLAT = document.getElementById('searchInputLAT').value;
-        let cityLNG = document.getElementById('searchInputLNG').value;
-        let newEntry = {
-            "searchCity": searchCity,
-            "cityLAT": cityLAT,
-            "cityLNG": cityLNG
-        }
+        if (typeof cityId === typeof undefined) {
+            let searchCity = document.getElementById("searchInput").value.trim();
+            cityId = document.getElementById("searchInputId").value;
+            let cityLAT = document.getElementById('searchInputLAT').value;
+            let cityLNG = document.getElementById('searchInputLNG').value;
+            let newEntry = {
+                "cityId": cityId,
+                "cityName": searchCity,
+                "cityLAT": cityLAT,
+                "cityLNG": cityLNG
+            }
 
-        // Check if an item already exists in the array and either add new or delete and add
-        let validateCity = searchItems.filter(city =>(city.searchCity === searchCity));
-        let cityIndex = searchItems.findIndex(city => city.searchCity === searchCity);
-        if (validateCity.length === 0 && newEntry.searchCity.length > 0) {
-            // Does not exist in array so add
-            searchItems.push(newEntry);
-        } else if (newEntry.searchCity.length > 0 && cityIndex != -1) {
-            // Does exist in the array so delete and add
-            searchItems.splice(searchCity, 1);
-            searchItems.push(newEntry);
-        } else if (cityIndex != -1){
-            // Delete entry in the array
-            searchItems.splice(searchCity, 1);
-        }
-        
-        // Store changes back to local storage
-        localStorage.setItem("weatherDashboard_searchItems", JSON.stringify(searchItems));
+            // Check if an item already exists in the array and either add new or delete and add
+            let validateCity = searchItems.filter(city =>(city.cityId === cityId));
+            let cityIndex = searchItems.findIndex(city => city.cityId === cityId);
+            if (validateCity.length === 0 && newEntry.cityId.length > 0) {
+                // Does not exist in array so add
+                searchItems.push(newEntry);
+            } else if (newEntry.cityId.length > 0 && cityIndex != -1) {
+                // Does exist in the array so delete and add
+                searchItems.splice(searchCity, 1);
+                searchItems.push(newEntry);
+            } else if (cityIndex != -1){
+                // Delete entry in the array
+                searchItems.splice(searchCity, 1);
+            }
+            
+            // Store changes back to local storage
+            localStorage.setItem("weatherDashboard_searchItems", JSON.stringify(searchItems));
+    
+            // Render Search history
+            renderSearchHistory();
 
-        // Render Search history
-        renderSearchHistory();
+        } 
+
+        // Render Results column
+        renderResults(cityId);
     }
 })
 
 google.maps.event.addListener(autocomplete, "place_changed", function() {
     let searchPlace = autocomplete.getPlace();
 
+    console.log(searchPlace);
+
+    document.getElementById('searchInputId').value = searchPlace.place_id;
     document.getElementById('searchInputLAT').value = searchPlace.geometry.location.lat();
     document.getElementById('searchInputLNG').value = searchPlace.geometry.location.lng();
+    
 
 })
 
